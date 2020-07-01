045ffdf709ca6857c702c911af64b94a090d90c4
BobNab <45999993+BobNab@users.noreply.github.com>
2020-07-01 08:37:19 +0200

    update

---
+++ index.php
@@ -0,0 +1,10 @@
+<?php
+
+// include header
+require("./standard_content/header.php");
+
+// include content with an handler
+require("./content/handler.php");
+
+// include footer
+require("./standard_content/footer.php");
--- standard_content/header.php
+++ standard_content/header.php
@@ -13,8 +13,6 @@
     <title></title>
   </head>
   <body>
-    <h1>Hello, world!</h1>
-
     <!-- Optional JavaScript -->
     <script src="https://kit.fontawesome.com/a076d05399.js"></script>
 
@@ -24,5 +22,7 @@
     <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js" integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI" crossorigin="anonymous"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
-  </body>
-</html>
+
+<?php
+session_start();
+require("./standard_content/navbar.php");
